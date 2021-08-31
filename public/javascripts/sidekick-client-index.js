f630b222fa0178c78f344c9a2347e2ede5b01fa0
Keith Harshaw <kharshaw@frontlineed.com>
2021-08-31 11:37:35 -0400

    split out js files

---
+++ public/javascripts/sidekick-client-index.js
@@ -0,0 +1,26 @@
+// do stuff
+
+var impersonation = false;
+var gemCount = 0;
+
+function something() {
+console.log('activity started');
+sidekick.events.emit('activity-indicator.visible', true);
+
+setTimeout(function () {
+    console.log('activity stopped');
+    sidekick.events.emit('activity-indicator.visible', false);
+}, 5000);
+
+}
+
+function toggleImpersonation() {
+impersonation = !impersonation;
+sidekick.events.emit('header.impersonation', impersonation);
+document.getElementById('btnImpersonate').innerText = (impersonation ? 'End Impersonation' : 'Impersonate');
+}
+
+function updateGem(addGemAmount) {
+gemCount += addGemAmount;
+sidekick.events.emit('gem-count', gemCount);
+}
