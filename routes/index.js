f5e054e55910bcdfbcacdfa83bed1a4729f0d811
Keith Harshaw <kharshaw@frontlineed.com>
2023-02-13 10:11:20 -0500

    add some org data

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.header('x-sidekick-org', '{"orgs": [{"id": "a", "name":"TestOrgA"},{"id": "a", "name":"TestOrgA"}]');
   res.render('index', { title: 'Home' });
 });
 
